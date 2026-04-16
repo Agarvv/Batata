f9eb613f93eea2d14d043c273b78ad0a580c16db
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-04-16 01:11:28 +0000

    homepage

---
+++ frontend/batata/src/pages/homepage/homepage.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+import './homepage.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='app'>
+           <div className='form-container'>
+                <div className='form'>
+                   <div className='logo'>
+                     <h1>Batata</h1>
+                   </div>
+
+                   <div>
+                    
+                   </div>
+                </div>
+            </div>
+     </div>
+    </> 
+  )
+}
+
+export default App
