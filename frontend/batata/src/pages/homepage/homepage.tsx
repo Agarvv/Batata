af9d43c083d63d68713652eff02d4a167e72693f
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-04-17 01:15:48 +0000

    designing home page

--- frontend/batata/src/pages/homepage/homepage.tsx
+++ frontend/batata/src/pages/homepage/homepage.tsx
@@ -12,12 +12,47 @@ function App() {
            <div className='form-container'>
                 <div className='form'>
                    <div className='logo'>
-                     <h1>Batata</h1>
+                     <p>Batata <strong> Safe Place</strong></p>
                    </div>
+                  
+                <div className='f'>
+                
+                  <div className='form-content'>
+                      <div className='fc-h'>
+                         <h2>JUGAAAAAAAR</h2>
+                      </div>
+                      
+                      <div className='inps'>
+                        <div className='inp'>
+                           <input type="text" placeholder='aqui va tu nombre uwu'/>
+                        </div>
+                        <div className='inp'>
+                           <input type="text" placeholder='aqui va un codigo de sala :v'/>
+                        </div>
+                      </div>
+                      
+                      <div className='btn'>
+                         <button>YAAAA WEEE</button>
+                      </div>
+                  </div>
 
-                   <div>
-                    
+                   <div className='tuto'>
+                      <h2>No sabes jugar? aqui te explico (gil)</h2>
+                      <p>Entras al juego, el sistema  te asignara a ti
+                         <br></br> (todo bien sigma q eres muak) a un equipo aleatorio de x personas. <br></br>
+                         ok?????? entiendes o no de momento???? *se le cae una tet- ok no :v <br></br>
+                         tu junto con esas personas con las que te toco, van a dibujar juntitos en equipo <br></br>
+                         tendras 5 minutos para dibujar con tu equipo asi q VE RAPIDO, MIER- ok no ya paro <br></br>
+                         tambien podreis coordinaros con un chat en tiempo real q yo (agarvv el teton) os prepare <br></br>
+                         cuando el tiempo se acabe, y todos acaben sus dibujos, <br></br>
+                         vais a votar cada uno de los dibujos. gana el equipo q mas votos tenga uwu  <br></br> 
+                         ES SIMPLE A Q SI?????????? diviertete q para eso lo hice ♡♡♡ os amo a todos ♡♡♡♡♡♡
+                         </p>
                    </div>
+                  </div>
+
+
+
                 </div>
             </div>
      </div>
