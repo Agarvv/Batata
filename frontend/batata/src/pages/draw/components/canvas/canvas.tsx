54a2721edd399e34dc9a30bebeb1666c2dc5ac4c
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-05-08 22:49:38 +0000

    draw page

--- frontend/batata/src/pages/draw/components/canvas/canvas.tsx
+++ frontend/batata/src/pages/draw/components/canvas/canvas.tsx
@@ -6,11 +6,40 @@ function Canvas() {
         <div className="canvas">
           <div className="c">
              <div className='canvas-area'>
+                  <div className='ca-header'>
+                    <h1>Dibuja, Cohone.</h1>
+                  </div>
 
+                  <div className="ca-canvas">
+                    <canvas>
+
+                    </canvas>
+                  </div>
+
+                  <div className="ca-footer">
+                      <div className='current-color'>
+
+                      </div>
+                    <div className="c-footer-c-options">
+                
+                          <div className='colors'>
+                             <div>
+
+                             </div>
+                          </div>
+                          <div className='tools'>
+
+                          </div>
+                    </div>  
+                  </div>
              </div>
 
              <div className="chat">
 
+
+                <div className='c-footer'> 
+                
+                </div> 
              </div>
           </div>
         </div>
