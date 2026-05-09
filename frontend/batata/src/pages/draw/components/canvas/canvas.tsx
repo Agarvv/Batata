19d238df966ec2f85f046edcb2666d72a1610674
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-05-09 23:50:24 +0000

    Draw Page

--- frontend/batata/src/pages/draw/components/canvas/canvas.tsx
+++ frontend/batata/src/pages/draw/components/canvas/canvas.tsx
@@ -19,26 +19,166 @@ function Canvas() {
                   <div className="ca-footer">
                       <div className='current-color'>
 
+                        <div>
+
+                        </div>
+                 
+
                       </div>
                     <div className="c-footer-c-options">
                 
                           <div className='colors'>
-                             <div>
-
-                             </div>
+                       
+                                <div className="red"></div>
+<                               div className="blue"></div>
+<div className="green"></div>
+<div className="yellow"></div>
+<div className="orange"></div>
+<div className="purple"></div>
+<div className="pink"></div>
+<div className="brown"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="gray"></div>
+<div className="cyan"></div>
+<div className="lime"></div>
+<div className="lavender"></div>
+                         
                           </div>
-                          <div className='tools'>
 
+
+                          <div className='tools'>
+                              <div className="pencil">
+                                 <i className="fas fa-pencil-alt"></i>
+                              </div>
+                              <div className="eraser">
+                                    <i className="fas fa-eraser"></i>
+                              </div>
+                              <div className="bucket">
+                                    <i className="fas fa-fill-drip"></i>
+                              </div>
+                              <div className="t1">
+                                    <div className="t1-cube">
+
+                                    </div>
+                              </div>
+                              <div className="t2">
+                                <div className="t2-cube">
+
+                                    </div>
+                              </div>
+                              <div className="t3">
+                                 <div className="t3-cube">
+
+                                    </div>
+                              </div>
+                              <div className="t4">
+                                 <div className="t4-cube">
+
+                                    </div>
+                              </div>
+                              <div className="delete">
+                                    <i className="fas fa-trash"></i>
+                              </div>
                           </div>
                     </div>  
                   </div>
              </div>
 
              <div className="chat">
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?</p>
+                    </div> 
+                </div>
 
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?mo estásmo estásmo estásmo estásmo estásmo estás</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?mo estásmo estásmo estásmo estásmo estásmo estás</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?mo estásmo estásmo estásmo estásmo estásmo estás</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?mo estásmo estásmo estásmo estásmo estásmo estás</p>
+                    </div> 
+                </div>
+
+                <div className="chat-msg">
+                   <div className='msg-user'>
+                       <img src="src/assets/abstract-technology-blue-curve-modern-600nw-2720628121.webp" alt="" />
+                    </div>
+                    <div className="msg-content"> 
+                        <p>Hola, ¿Cómo estás?mo estásmo estásmo estásmo estásmo estásmo estás</p>
+                    </div> 
+                </div>
 
                 <div className='c-footer'> 
-                
+                   <input type="text" placeholder='Di Algo We'/>
+                   <i className="fa fa-send"></i> 
                 </div> 
              </div>
           </div>
