5130bd138773889341b9991e6381b56a022343c7
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-07-05 23:19:38 +0000

    images

--- frontend/batata/src/main.tsx
+++ frontend/batata/src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from "react"
+import { StrictMode, useEffect } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, useRoutes } from "react-router-dom"
+import { BrowserRouter, useLocation, useRoutes } from "react-router-dom"
 import { routes } from "./routes"
 import './index.css'
 
@@ -9,9 +9,20 @@ function Router() {
   return element
 }
 
+function RouteBackground() {
+  const location = useLocation()
+
+  useEffect(() => {
+    document.body.dataset.route = location.pathname
+  }, [location.pathname])
+
+  return null
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <RouteBackground />
       <Router />
     </BrowserRouter>
   </StrictMode>
